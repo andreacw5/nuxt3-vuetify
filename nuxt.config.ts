49543f132d51bcacf72ecec2c51d3f203648fd00
Andrea Tombolato <andreacw96@gmail.com>
2024-04-29 18:24:51 +0200

    Eslint and layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,11 @@ app: {
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
-      title: "nuxt3-vuetify",
+      title: "Nuxt Example",
       titleTemplate: "%s | nuxt3-vuetify",
     },
   },
+  modules: ['@nuxtjs/sitemap', '@nuxtjs/eslint-module'],
   css: ["vuetify/lib/styles/main.sass", '@mdi/font/css/materialdesignicons.css'],
   build: {
     transpile: ["vuetify"],
